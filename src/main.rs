72cbb9f1c753b1d5f80e11cf4840652951b3a313
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-12-14 23:16:16 -0300

    log when running

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{routing::get, Router};
+use std::net;
 
 mod handlers;
 
@@ -6,7 +7,10 @@ mod handlers;
 async fn main() {
     let app = Router::new().route("/", get(handlers::hello_notetaker));
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    let addr = net::SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on: {addr}");
+
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
