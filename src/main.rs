62ba068a3ae39db88a09789fd54493854ece7d8a
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-12-28 16:45:39 -0300

    configure tracing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use axum::{routing::get, Router};
 use std::net;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod handlers;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "notetaker_api=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer().compact())
+        .init();
+
     let app = Router::new()
         .route("/", get(handlers::hello_notetaker))
         .route("/hello_name", get(handlers::hello_name))
@@ -12,10 +22,11 @@ async fn main() {
             "/notes",
             get(handlers::list_notes).post(handlers::create_note),
         )
-        .route("/notes/:note_id", get(handlers::get_note));
+        .route("/notes/:note_id", get(handlers::get_note))
+        .layer(TraceLayer::new_for_http());
 
     let addr = net::SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on: {addr}");
+    tracing::debug!("Listening on: {addr}");
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
