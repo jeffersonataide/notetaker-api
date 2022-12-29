26d27af80cc28139d44c0bf720a5b3317a1f11aa
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-12-28 22:15:32 -0300

    create app function

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,18 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer().compact())
         .init();
 
-    let app = Router::new()
+    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let addr = net::SocketAddr::from_str(&format!("0.0.0.0:{port}")).unwrap();
+    tracing::debug!("Listening on: {addr}");
+
+    axum::Server::bind(&addr)
+        .serve(app().into_make_service())
+        .await
+        .unwrap();
+}
+
+fn app() -> Router {
+    Router::new()
         .route("/", get(handlers::hello_notetaker))
         .route("/hello_name", get(handlers::hello_name))
         .route(
@@ -24,14 +35,5 @@ async fn main() {
             get(handlers::list_notes).post(handlers::create_note),
         )
         .route("/notes/:note_id", get(handlers::get_note))
-        .layer(TraceLayer::new_for_http());
-
-    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
-    let addr = net::SocketAddr::from_str(&format!("0.0.0.0:{port}")).unwrap();
-    tracing::debug!("Listening on: {addr}");
-
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .layer(TraceLayer::new_for_http())
 }
